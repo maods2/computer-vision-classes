dd31cad8618e4c0c4fab8207762dae0ba26e31c4
Matheus Augusto <matheusalgusto_@hotmail.com>
2022-09-19 23:58:52 -0300

    New processing

--- utils.py
+++ utils.py
@@ -3,17 +3,43 @@ import numpy as np
 from skimage.io import imread, imsave
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+from typing import Callable
 
 
 def transform_and_store(
     main_path: str,
     data_folder_name: str, 
     new_folder_name: str, 
-    transformation_function
+    transformation_function: Callable
     ) -> None:
-    """Function responsible for mapping
-    
+    """Utility functions that will load data from a given directoty,
+    process this data based on a given transformation function and store 
+    the output into a new folder also passed as argument of this function.
+
+    Parameters
+    ----------
+    main_path : string
+        Main directory where the data is stored.
+    data_folder_name : string
+        Data folder name in which the data that will be processed is stored.
+    new_folder_name : string
+        Name of the new folder that will be created in order to store the
+        output data.
+    transformation_function : Callable-like.
+        Transformation functions that shoul be applyed to the input data 
+
+
+    Examples
+    --------
+
+    >>> transform_and_store(
+            main_path='./main_path/',
+            data_folder_name='00-original', 
+            new_folder_name='01-resized', 
+            transformation_function=apply_resizing
+        )
     """
+    SUB_FOLDERS = ['/n/', '/p/']
 
     transformation_path_n = pathlib.Path(main_path + new_folder_name + '/n/')
     transformation_path_p = pathlib.Path(main_path + new_folder_name + '/p/')
@@ -21,8 +47,8 @@ def transform_and_store(
     transformation_path_n.mkdir(parents=True, exist_ok=True)
     transformation_path_p.mkdir(parents=True, exist_ok=True)
 
-    sub_folders = ['/n/', '/p/']
-    for sub_folder in sub_folders:
+
+    for sub_folder in SUB_FOLDERS:
         data_path = pathlib.Path(main_path + data_folder_name + sub_folder)
         for path in data_path.glob('*'):
             input_path = str(path)
@@ -39,7 +65,27 @@ def plot_transformation_comparison(
         data_folder_name: str, 
         new_folder_name: str, 
     ) -> None:
+    """Utility function that will plot images present into given directories.
 
+    Parameters
+    ----------
+    main_path : string
+        Main directory where the data is stored.
+    data_folder_name : string
+        Data folder name in which the data that will be plotted is stored.
+    new_folder_name : string
+        Data folder name in which the data that will be plotted is stored.
+   
+
+    Examples
+    --------
+
+    >>> plot_transformation_comparison(
+            main_path='/main_path/',
+            data_folder_name='00-original', 
+            new_folder_name='01-resized',
+        )
+    """
     path_original = list(pathlib.Path(main_path + data_folder_name + '/n/' ).glob('*'))
     path_transformed = list(pathlib.Path(main_path + new_folder_name + '/n/' ).glob('*'))
     n_row, n_col = 2, 4
